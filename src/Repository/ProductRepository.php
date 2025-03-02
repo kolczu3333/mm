f262f93e038a31d970537a7c0a7c394cf07017c9
Kamil Kołcz <kolczu@tlen.pl>
2025-03-02 13:55:23 +0100

    -1

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,19 +3,48 @@
 namespace App\Repository;
 
 use App\Entity\Product;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Exception;
 
-class ProductRepository extends ServiceEntityRepository
+readonly class ProductRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(private EntityManagerInterface $em)
     {
-        parent::__construct($registry, Product::class);
     }
 
     public function save(Product $product): void
     {
-        $this->getEntityManager()->persist($product);
-        $this->getEntityManager()->flush();
+        $this->em->persist($product);
+        $this->em->flush();
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     * @throws Exception
+     */
+    public function update(Product $product): void
+    {
+        $existingProduct = $this->em->find(Product::class, $product->getId());
+        if ($existingProduct) {
+            $existingProduct->setName($product->getName());
+            $existingProduct->setPrice($product->getPrice());
+            $existingProduct->setStock($product->getStock());
+
+            $this->em->flush();
+        } else {
+            throw new Exception('Product not found');
+        }
+    }
+
+    public function findByExternalId(string $externalId):?Product
+    {
+        $query = $this->em->createQuery(
+            'SELECT p FROM App\Entity\Product p where p.externalId = :externalId'
+        )->setParameter('externalId', $externalId);
+
+        return $query->getOneOrNullResult();
     }
 }
